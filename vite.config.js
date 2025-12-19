b156e9fcd2e47bac0e83d6c8124dbe2c6778f60c
serii <seriiburduja@gmail.com>
2025-12-19 20:36:09 +0200

    Initial commit

---
+++ vite.config.js
@@ -0,0 +1,13 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+import handlebars from "vite-plugin-handlebars";
+
+export default defineConfig({
+  base: "./",
+  plugins: [
+    handlebars({
+      partialDirectory: resolve(__dirname, "partials"),
+    }),
+  ],
+});
